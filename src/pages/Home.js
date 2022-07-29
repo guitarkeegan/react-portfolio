dd3ba77e88b2270eb70fb9a011d26346faf3b5cd
guitarkeegan <keegananglim@gmail.com>
2022-07-28 20:52:49 -0700

    style homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="container text-center">
+      <div className="title">
+        <h1>Keegan Anglim</h1>
+      </div>
+      <div>
+        <h2>Full Stack Engineer</h2>
+      </div>
+    </main>
+  );
+}
