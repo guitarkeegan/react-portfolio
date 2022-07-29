b2c30f705a7e4f1c09ddeb08378e6a0903d62468
guitarkeegan <keegananglim@gmail.com>
2022-07-28 21:55:09 -0700

    add about page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Home from "./pages/Home";
-import Header from "./pages/Header"
+import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
 
 // Our App component returns the Greeting component
 function App() {
   return (
     <>
-  <Header />
-  <Home />
-  <Footer />
-  </>
-  )
+      <Header />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import profile from '../components/img/profile_selfie.jpg';
+
+export default function About(){
+
+    const styles = {
+        img: {
+            maxWidth: "400px",
+            border: "solid #B8BFBD 5px"
+        }
+    }
+
+    return (
+        <div className='about-container container mt-5'>
+        <div className='about-title-div'>
+            <div className='row'>
+                <div className='col-6 text-center p-1'>
+                <img style={styles.img} className='img-fluid' src={profile}></img>
+                </div>
+                <div className='col-6'>
+                <p>I moved to Southern California in 2003 to study jazz guitar at the University of Southern California. Since graduating in 2012 with a Doctorate of Music, I have been teaching a music courses in higher education at colleges throughout Los Angeles.</p>
+                <p>I completed a full stack web developement bootcamp through UCLA-Extention on August 9, 2022. I am now interested in taking on a new challege as a full stack web engineer.</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
